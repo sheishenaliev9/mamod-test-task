37c13f3a142c1826488595f93ee72b5f3c6a6276
o.islam <i.orozobaev.xyz@gmail.com>
2024-03-08 20:12:52 +0600

    finished styling

--- src/components/UI/CustomInput.vue
+++ src/components/UI/CustomInput.vue
@@ -1,5 +1,14 @@
 <template>
-  <input class="custom-input" :type="type" :placeholder="placeholder" />
+  <div class="input-container">
+    <input class="custom-input" :type="inputType" :placeholder="placeholder" />
+    <img
+      v-if="showIcon"
+      :src="iconStateType"
+      alt="eye-icon"
+      class="eye-icon"
+      @click="toggleInputType"
+    />
+  </div>
 </template>
 
 <script>
@@ -8,15 +17,40 @@ export default {
   props: {
     type: String,
     placeholder: String,
+    showIcon: Boolean,
+  },
+  data() {
+    return {
+      inputType: this.type,
+    };
+  },
+  methods: {
+    toggleInputType() {
+      this.inputType = this.inputType === "password" ? "text" : "password";
+      this.$emit("toggle-password");
+    },
+  },
+  computed: {
+    iconStateType() {
+      return this.inputType === "text"
+        ? require("../../assets/icons/hided.svg")
+        : require("../../assets/icons/showed.svg");
+    },
   },
 };
 </script>
 
-<style>
+<style scoped>
+.input-container {
+  position: relative;
+  width: 450px;
+}
+
 .custom-input {
+  outline: none;
   padding-left: 10px;
   border: 1px solid #d9d9d9;
-  width: 450px;
+  width: 97%;
   height: 40px;
   border-radius: 15px;
 }
@@ -25,5 +59,13 @@ export default {
   font-size: 14px;
   letter-spacing: 1px;
   color: #9292a0;
+}
+
+.eye-icon {
+  position: absolute;
+  cursor: pointer;
+  right: 10px;
+  top: 50%;
+  transform: translateY(-50%);
 }
 </style>
